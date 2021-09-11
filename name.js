377a1b5e444149a8fde011204e1f27a3451b1f62
ramon <monchouam@gmail.com>
2021-09-10 21:20:23 -0500

    practicing with html and javascript to add two numbers in a simple form

--- name.js
+++ name.js
@@ -1,10 +1,15 @@
-// aca codigo javascript - Ramón, crea una variable y asignale tu nombre
-// segun las indicaciones.s
+function calcSum() {
+    let a = document.getElementById("n1").value
+    let b = document.getElementById("n2").value
 
-let name = "ramón"
-console.log(name)
+    if (a.length > 0 && b.length > 0) {
+        a = parseFloat(a)
+        b = parseFloat(b)
+        if (!isNaN(a) && !isNaN(b))
+            document.getElementById("res").innerHTML = `${a} + ${b} = ${a + b}`
+        else
+            document.getElementById("res").innerHTML = "all fields must be number"
+    } else
+        document.getElementById("res").innerHTML = "any field can't be empty"
 
-let a = parseFloat(prompt("first number: "))
-let b = parseFloat(prompt("second number: "))
-
-alert(`${a} + ${b} = ${a+b}`)
+}
